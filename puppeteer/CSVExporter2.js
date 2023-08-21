e08114b6d2dba317091c6346727b80a560075210
Eddie Hsu <aoe102198@gmail.com>
2023-08-22 00:51:05 +0800

    Update exporter2

--- puppeteer/CSVExporter2.js
+++ puppeteer/CSVExporter2.js
@@ -1,12 +1,13 @@
 const HCCrawler = require("headless-chrome-crawler");
 const CSVExporter = require("headless-chrome-crawler/exporter/csv");
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 const FILE = "./tmp/result.csv";
 
 const exporter = new CSVExporter({
   file: FILE,
-  fields: ["title", "link"],
+  fields: ["image", "link", "response.url"],
   separator: "\t",
 });
 
@@ -17,12 +18,18 @@ const exporter = new CSVExporter({
   const crawler = await HCCrawler.launch({
     maxDepth: 1,
     exporter,
-    evaluatePage: () => ({
-      title: document.title,
-      link: window.location.href,
-    }),
-    onSuccess: result => {
-      console.log(`Scraped ${result.result.title}`);
+    evaluatePage: () => {
+      const imageElement = document.getElementsByClassName(
+        "Pos(a) W(100%) H(100%)"
+      )[0];
+      const imageSrc = imageElement ? imageElement.getAttribute("src") : "";
+      return {
+        image: imageSrc,
+        link: window.location.href,
+      };
+    },
+    onSuccess: (result) => {
+      console.log(`Scraped ${result.result.image} | ${result.result.link}`);
     },
   });
 
@@ -34,4 +41,8 @@ const exporter = new CSVExporter({
   await crawler.onIdle();
   await crawler.close();
   await browser.close();
+
+  // 將資料寫入 CSV 檔案
+  const csvData = fs.readFileSync(FILE, "utf-8");
+  console.log(csvData); // 將寫入的資料印出來
 })();
