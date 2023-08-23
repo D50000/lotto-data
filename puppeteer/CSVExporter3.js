2f4fcab10e792c0fe80832d6bc4192bd24e22a1d
Eddie Hsu <aoe102198@gmail.com>
2023-08-24 00:53:43 +0800

    Update news crawler.
    1. Export csv into "archived" directory.
    2. Crawler news' photos & titles.

--- puppeteer/CSVExporter3.js
+++ puppeteer/CSVExporter3.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-const FILE = "./tmp/result.csv";
+const FILE = "./archived/result.csv";
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -10,26 +10,25 @@ const FILE = "./tmp/result.csv";
   await page.goto("https://tw.news.yahoo.com/");
 
   // Selector/Crawler.
-  // Get the news photo
-  const imageElements1 = await page.evaluate(() => {
+  // Get the news photo and title.
+  const newsElements = await page.evaluate(() => {
     const elements = document.querySelectorAll(
-      ".Pos\\(r\\).H\\(100\\%\\).C\\(\\#fff\\).Td\\(u\\)\\:h > img"
-    ); // escape character
-    return Array.from(elements).map((element) => element.getAttribute("src"));
-  });
-  // Get the news photo's title.
-  const imageElements2 = await page.evaluate(() => {
-    const elements = document.querySelectorAll(
-      ".Pos\\(r\\).H\\(100\\%\\).C\\(\\#fff\\).Td\\(u\\)\\:h > div > a"
-    ); // escape character
-    return Array.from(elements).map((element) => element.getInnerHTML());
+      ".Pos\\(r\\).H\\(100\\%\\).C\\(\\#fff\\).Td\\(u\\)\\:h"
+    );
+    // return {"image": "src", "title":"string"}
+    return Array.from(elements).map((element) => ({
+      image: element.querySelector("img").getAttribute("src"),
+      title: element.querySelector("div > a").innerText,
+    }));
   });
 
   // CSV header.
-  const csvHeader = "Image URL";
+  const csvHeader = "Image URL,Title";
   // Write the crawled data into csv.
-  const csvData = imageElements1.map((image) => `"${image}"`).join("\n");
-  fs.writeFileSync(FILE, csvData, "utf-8");
+  const csvData = newsElements
+    .map((news) => `"${news.image}","${news.title}"`)
+    .join("\n");
+  fs.writeFileSync(FILE, `${csvHeader}\n${csvData}`, "utf-8");
 
   await browser.close();
 })();
