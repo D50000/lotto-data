570dfc58de7eaa97627842f8983631e271dbf263
Eddie Hsu <aoe102198@gmail.com>
2023-08-27 23:38:53 +0800

    Add the Comment and Doc for the example.

--- puppeteer/CSVExporter3.js
+++ puppeteer/CSVExporter3.js
@@ -4,9 +4,11 @@ const fs = require("fs");
 const FILE = "./archived/result.csv";
 
 (async () => {
+  // Puppeteer will lunch Chromium or Chrome do the headless imitation.
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
+  // Load the target URL.
   await page.goto("https://tw.news.yahoo.com/");
 
   // Selector/Crawler.
@@ -16,14 +18,14 @@ const FILE = "./archived/result.csv";
     const elements = document.querySelectorAll(
       ".Pos\\(r\\).H\\(100\\%\\).C\\(\\#fff\\).Td\\(u\\)\\:h"
     );
-    // return {"image": "src", "title":"string"}
+    // return json object {"image": "src", "title":"string"}.
     return Array.from(elements).map((element) => ({
       image: element.querySelector("img").getAttribute("src"),
       title: element.querySelector("div > a").innerText,
     }));
   });
 
-  // CSV header.
+  // Config CSV header.
   const csvHeader = "Image URL,Title";
   // Write the crawled data into csv.
   const csvData = newsElements
--- puppeteer/readme.md
+++ puppeteer/readme.md
@@ -4,4 +4,3 @@
 
 - Complete the guideline.
 - "News crawler" example need to supporter interact with infinity scroll then crawl the element data.
-  - Comment and Doc for the example.
