0ce53382825df2d957d522c378ff890a9546d914
Eddie Hsu <aoe102198@gmail.com>
2023-09-10 00:30:38 +0800

    Beta crawler version.
    Input query date and submit.
    Get the 'Lotto649Control_history_dlQuery_L649_DrawTerm_0' data and writhe it into CSV file.

--- puppeteer/taiwanLottery_exporCSVt.js
+++ puppeteer/taiwanLottery_exporCSVt.js
@@ -13,48 +13,57 @@ const FILE = "./tmp/taiwanLottery.csv";
 
   const results = [];
 
-  for (let issue = startIssue; issue <= endIssue; issue++) {
-    const url = `https://www.taiwanlottery.com.tw/Lotto/Lotto649/history.aspx`;
+  const url = `https://www.taiwanlottery.com.tw/Lotto/Lotto649/history.aspx`;
 
-    await page.goto(url);
+  await page.goto(url);
 
-    const data = await page.evaluate(() => {
-      // Setup query historyNo.
-      document.getElementById("Lotto649Control_history_txtNO").value =
-        startIssue;
-      const rowData = Array.from(document.querySelectorAll(".tableFull tr"))
-        .slice(1)
-        .map((row) => {
-          const columns = Array.from(row.querySelectorAll("td")).map((td) =>
-            td.innerText.trim()
-          );
-          return columns;
-        });
+  const data = await page.evaluate(() => {
+    // Setup query historyNo.
+    const queryInputBox = document.getElementById(
+      "Lotto649Control_history_txtNO"
+    );
+    queryInputBox.value = startIssue;
+    // Submit for query.
+    const submitBtn = document.getElementById(
+      "Lotto649Control_history_btnSubmit"
+    );
+    submitBtn.click();
 
-      return rowData;
-    });
+    const test = document.getElementById(
+      "Lotto649Control_history_dlQuery_L649_DrawTerm_0"
+    ).textContent;
+    return test;
+  });
 
-    if (data.length > 0) {
-      results.push(data[0]);
-      console.log(`Scraped issue ${issue}`);
-    }
-  }
+  console.log(`Test data result: ${data}`);
 
   await browser.close();
 
   // Write the data to CSV file
   const csvStream = csv.format({
+    // 期別 Lotto649Control_history_dlQuery_L649_DrawTerm_0
+    // 開獎日 Lotto649Control_history1_dlQuery_ctl00_L649_DDate
+    // 頭獎 Lotto649Control_history_dlQuery_L649_CategA5_0
+    // 獎金總額 Lotto649Control_history_dlQuery_Total_0
+    // 獎號 Lotto649Control_history_dlQuery_No1_0,
+    //     Lotto649Control_history_dlQuery_No2_0
+    //     Lotto649Control_history_dlQuery_No3_0
+    //     Lotto649Control_history_dlQuery_No4_0
+    //     Lotto649Control_history_dlQuery_No5_0
+    //     Lotto649Control_history_dlQuery_No6_0
+    // 特別號 Lotto649Control_history_dlQuery_SNo_0
     headers: ["期別", "開獎日", "頭獎", "獎金總額", "獎號", "特別號"],
   });
   const writableStream = fs.createWriteStream(FILE, "utf-8");
 
   csvStream.pipe(writableStream);
 
-  results.forEach((result) => {
-    csvStream.write(result);
-  });
+  // results.forEach((result) => {
+  //   csvStream.write(result);
+  // });
+  csvStream.write(data);
 
   csvStream.end();
 
-  console.log("Data has been saved to", FILE);
+  console.log("History data has been saved to", FILE);
 })();
