c5af83e64b77ebd5053158e29955933b35f5f019
Eddie Hsu <aoe102198@gmail.com>
2023-10-05 23:52:37 +0800

    Archived 111y data.

--- puppeteer/taiwanLottery_exporCSVt.js
+++ puppeteer/taiwanLottery_exporCSVt.js
@@ -2,15 +2,15 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const csv = require("fast-csv");
 
-const FILE = "./archived/taiwanLottery_110.csv";
+const FILE = "./archived/taiwanLottery_111.csv";
 
 (async () => {
   try {
     const browser = await puppeteer.launch({ headless: false }); // 使用有界面的 Chrome，以便觀察操作
     const page = await browser.newPage();
     // Setup
-    const startIssue = 110000001; 
-    const endIssue = 110000114; // Adjust this range as needed.
+    const startIssue = 111000001;
+    const endIssue = 111000114; // Adjust this range as needed.
     const url = `https://www.taiwanlottery.com.tw/Lotto/Lotto649/history.aspx`;
 
     // Write the data to CSV file
